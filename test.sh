d496a23feae6ef0dab2b7891c0ca0bc9aaf4d8f1
Ben Maddison <benm@workonline.co.za>
2019-03-28 23:29:09 +0200

    test script improvements

--- test.sh
+++ test.sh
@@ -19,18 +19,29 @@ clean() {
       ok || fail 1
   fi
 }
-# IMAGE="ceos-prefix-list-agent-test"
-# CONTAINER_NAME="${IMAGE}-${VERSION}"
+
+# Prepare test container
 echo -n "Building test image"
 IMAGE_ID="$(docker build --build-arg REGISTRY="${REGISTRY}" --build-arg VERSION="${VERSION}" --quiet .)" &&
   ok || fail
 echo -n "Starting container"
 CONTAINER_ID="$(docker run --detach --privileged --rm "${IMAGE_ID}")" &&
   ok || fail
+
+# Run test suite
 echo "Running test suite"
 docker exec --interactive --tty "${CONTAINER_ID}" pytest
 echo -n "Retrieving coverage report"
 docker cp ${CONTAINER_ID}:/root/coverage.xml ./ >/dev/null &&
- ok || fail
+  ok || fail
+
+# Drop into a shell if requested
+if [[ -n "$1" ]]; then
+  docker exec --interactive --tty "${CONTAINER_ID}" /bin/bash ||
+    fail
+fi
+
+# Clean up
 clean
+
 exit 0
