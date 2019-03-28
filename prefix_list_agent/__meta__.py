4b5b70270d3a60c01237fdb62f92f37e1c832b0d
Ben Maddison <benm@workonline.co.za>
2019-03-29 00:29:36 +0200

    bump version

--- prefix_list_agent/__meta__.py
+++ prefix_list_agent/__meta__.py
@@ -15,7 +15,7 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.1.0a3"
+__version__ = "0.1.0a4"
 __author__ = "Ben Maddison"
 __author_email__ = "benm@workonline.africa"
 __licence__ = "MIT"
