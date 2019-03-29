64f6a77b5d5c3274b2a13b37a4b4a0534a38185f
Ben Maddison <benm@workonline.co.za>
2019-03-29 03:02:28 +0200

    add worker method tests

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -14,6 +14,11 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import StringIO
+import urllib2
+
+import pytest
+
 from prefix_list_agent.worker import PrefixListWorker
 
 
@@ -23,3 +28,34 @@ class TestPrefixListWorker(object):
     def test_init(self, worker):
         """Test case for PrefixListWorker initialisation."""
         assert isinstance(worker, PrefixListWorker)
+
+    @pytest.mark.parametrize("side_effect", (
+        (urllib2.addinfourl(url="/testing", code=200, headers=None,
+                            fp=StringIO.StringIO('{"foo":"bar"}')),),
+        pytest.param(urllib2.URLError(reason="Testing"),
+                     marks=pytest.mark.xfail(raises=urllib2.URLError,
+                                             strict=True)),
+        pytest.param(urllib2.HTTPError(url="/testing", code=500,
+                                       msg="Testing", hdrs=None, fp=None),
+                     marks=pytest.mark.xfail(raises=urllib2.HTTPError,
+                                             strict=True))
+    ))
+    def test_rptk_request(self, mocker, worker, side_effect):
+        """Test case for 'rptk_request' method."""
+        mocker.patch.object(urllib2, "urlopen", autospec=True,
+                            side_effect=side_effect)
+        result = worker.rptk_request("/testing")
+        assert result["foo"] == "bar"
+
+    @pytest.mark.parametrize("obj", (
+        '{"foo":"bar"}',
+        StringIO.StringIO('{"foo":"bar"}'),
+        pytest.param("foo",
+                     marks=pytest.mark.xfail(raises=ValueError, strict=True)),
+        pytest.param(StringIO.StringIO("foo"),
+                     marks=pytest.mark.xfail(raises=ValueError, strict=True))
+    ))
+    def test_json_load(self, worker, obj):
+        """Test case for 'json_load' method."""
+        result = worker.json_load(obj)
+        assert result["foo"] == "bar"
